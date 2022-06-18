e2cb612f104d9df4a3d0124d5bb7288a7b6474c3
saray <saraykim2013@gmail.com>
2022-06-18 09:56:16 +0700

    Update

---
+++ Extension.py
@@ -0,0 +1,102 @@
+
+
+# *---* CASCADING STYLE SHEETS START *---*
+'''
+1) Bootstrap 5 Quick Snippets
+3) 
+'''
+# *---* CASCADING STYLE SHEETS STOP *---*
+
+# *---* JAVASCRIPT START *---*
+'''
+1) Angular Language Service
+2) AngularJs 1.x Code Snippets
+3) ES7+ React/Redux/React-Native snippets
+4) JavaScript (ES6) code snippets
+5) jQuery Code Snippets
+6) Reactjs code snippets
+7) Simple React Snippets
+8) Vetur
+9) vue
+'''
+# *---* JAVASCRIPT STOP *---*
+
+# *---* VISUAL STUDIO CODE STYLE START *---*
+'''
+1) Auto Rename Tag
+2) Fluent Icons
+3) IntelliCode
+4) Live Server
+5) Prettier - Code formatter
+6) Remote - Containers
+7) vscode-icons
+8) Regex Previewer
+9) Better Comments
+10) Code Spell Checker
+11) Tabnine AI Autocomplete for Javascript, Python, Typescript, PHP, Go, Java, Ruby & more
+12) Auto Import
+13) Import Cost
+'''
+# *---* VISUAL STUDIO CODE STYLE STOP *---*
+
+# *---* PYTHON START *---*
+'''
+1) Django
+2) Jupyter
+3) Jupyter Keymap
+4) Jupyter Notebook Renderers
+5) Pylance
+6) Python
+
+'''
+# *---* PYTHON STOP *---*
+
+# *---* FLUTTER START *---*
+'''
+1) Awesome Flutter Snippets
+2) Dart
+3) Flutter
+4) bloc
+'''
+# *---* FLUTTER STOP *---*
+
+# *---* JAVA START *---*
+'''
+1) Extension Pack for Java
+2) Language Support for Java(TM) by Red Hat
+3) Debugger for Java
+4) Maven for Java
+5) Project Manager for Java
+6) Test Runner for Java
+'''
+# *---* JAVA STOP *---*
+
+# *---* GIT START *---*
+'''
+1) GitHub Pull Requests and Issues
+2) GitLens — Git supercharged
+'''
+# *---* GIT STOP *---*
+
+# *---* SQL START *---*
+'''
+1) Language PL/SQL
+2) GitLens — Git supercharged
+'''
+# *---* GIT STOP *---*
+
+# *---* PHP START *---*
+'''
+1) PHP Debug
+2) PHP Extension Pack
+3) PHP Intelephense
+4) PHP IntelliSense
+'''
+# *---* PHP STOP *---*
+
+
+
+
+
+
+
